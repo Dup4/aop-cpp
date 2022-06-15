c2b3fd7f7663088fc35a4f30fdb1d87ff6a979ab
Dup4 <lyuzhi.pan@gmail.com>
2022-06-15 10:06:04 +0800

    chore: init package name

--- test/hello_world_benchmark.cc
+++ test/hello_world_benchmark.cc
@@ -1,10 +1,7 @@
 #include <benchmark/benchmark.h>
 
-#include "decorator/hello_world.h"
-
 static void BenchmarkHelloWorld(benchmark::State& state) {
     for (auto _ : state) {
-        HelloWorld();
     }
 }
 
