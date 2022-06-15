047fff987061900411da10478920c068c37ca8e4
Dup4 <lyuzhi.pan@gmail.com>
2022-06-15 14:30:54 +0800

    feat: init decorator example

--- include/decorator/decorator.h
+++ include/decorator/decorator.h
@@ -1,4 +1,16 @@
 #ifndef DECORATOR_DECORATOR_H
 #define DECORATOR_DECORATOR_H
 
+template <typename T>
+class DecoratorBaseCRTP {
+public:
+    DecoratorBaseCRTP() {}
+    virtual ~DecoratorBaseCRTP() {}
+
+protected:
+    T& derived() {
+        return static_cast<T&>(*this);
+    }
+};
+
 #endif  // DECORATOR_DECORATOR_H
