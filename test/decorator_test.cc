cbdbd5c9829a020798f3e0ab03f9486cb547f287
Dup4 <lyuzhi.pan@gmail.com>
2022-09-01 09:53:07 +0800

    chore: adjust CMakeLists

--- test/decorator_test.cc
+++ test/decorator_test.cc
@@ -1,4 +1,5 @@
-#include <gtest/gtest.h>
+#include "gtest/gtest.h"
+#include "snapshot/snapshot.h"
 
 #include "decorator/decorator.h"
 
--- test/hello_world_benchmark.cc
+++ test/hello_world_benchmark.cc
@@ -1,4 +1,4 @@
-#include <benchmark/benchmark.h>
+#include "benchmark/benchmark.h"
 
 static void BenchmarkHelloWorld(benchmark::State& state) {
     for (auto _ : state) {
