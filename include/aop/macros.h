10d782e2cc51810e81c33843384c38593e1e3c57
Dup4 <lyuzhi.pan@gmail.com>
2022-09-02 09:33:01 +0800

    refactor: use std::decay_t

--- include/aop/macros.h
+++ include/aop/macros.h
@@ -7,25 +7,25 @@
 #include "./aop_execute_context.h"
 #include "./aop_utility.h"
 
-#define AOP_DECLARE_FUNC_BEGIN(func, ...)                                    \
-    using ReturnType = std::remove_reference_t<decltype(func(__VA_ARGS__))>; \
-    auto res = AOPUtility::GetInitValue<ReturnType>();                       \
-    auto ctx = AOPExecuteContext{};                                          \
-                                                                             \
-    [[maybe_unused]] auto proxy_args_func = std::invoke([&]() {              \
-        if constexpr (std::is_void_v<ReturnType>) {                          \
-            return AOPUtility::GetProxyFunc(ctx, __VA_ARGS__);               \
-        } else {                                                             \
-            return AOPUtility::GetProxyFunc(ctx, res, __VA_ARGS__);          \
-        }                                                                    \
-    });                                                                      \
-                                                                             \
-    auto before_func = []() {};                                              \
-    auto after_func = []() {};                                               \
-    auto execute_func = [&]() {                                              \
-        res = AOPUtility::GetExecuteFuncValue<ReturnType>([&]() {            \
-            return func(__VA_ARGS__);                                        \
-        });                                                                  \
+#define AOP_DECLARE_FUNC_BEGIN(func, ...)                           \
+    using ReturnType = std::decay_t<decltype(func(__VA_ARGS__))>;   \
+    auto res = AOPUtility::GetInitValue<ReturnType>();              \
+    auto ctx = AOPExecuteContext{};                                 \
+                                                                    \
+    [[maybe_unused]] auto proxy_args_func = std::invoke([&]() {     \
+        if constexpr (std::is_void_v<ReturnType>) {                 \
+            return AOPUtility::GetProxyFunc(ctx, __VA_ARGS__);      \
+        } else {                                                    \
+            return AOPUtility::GetProxyFunc(ctx, res, __VA_ARGS__); \
+        }                                                           \
+    });                                                             \
+                                                                    \
+    auto before_func = []() {};                                     \
+    auto after_func = []() {};                                      \
+    auto execute_func = [&]() {                                     \
+        res = AOPUtility::GetExecuteFuncValue<ReturnType>([&]() {   \
+            return func(__VA_ARGS__);                               \
+        });                                                         \
     };
 
 #define AOP_DECLARE_FUNC_ASPECT(aspect)                                                    \
