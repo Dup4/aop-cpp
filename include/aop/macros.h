69d8f78397405ca403076716c4d59c442f533a74
Dup4 <lyuzhi.pan@gmail.com>
2022-09-02 17:30:28 +0800

    fix: macors namespace

--- include/aop/macros.h
+++ include/aop/macros.h
@@ -8,44 +8,44 @@
 #include "./aop_execute_context.h"
 #include "./aop_utility.h"
 
-#define AOP_DECLARE_FUNC_BEGIN(ContextType, func, ...)              \
-    using ReturnType = std::decay_t<decltype(func(__VA_ARGS__))>;   \
-    auto res = AOPUtility::GetInitValue<ReturnType>();              \
-    auto ctx = ContextType{};                                       \
-                                                                    \
-    [[maybe_unused]] auto proxy_args_func = std::invoke([&]() {     \
-        if constexpr (std::is_void_v<ReturnType>) {                 \
-            return AOPUtility::GetProxyFunc(ctx, __VA_ARGS__);      \
-        } else {                                                    \
-            return AOPUtility::GetProxyFunc(ctx, res, __VA_ARGS__); \
-        }                                                           \
-    });                                                             \
-                                                                    \
-    std::function<void()> before_func = []() {};                    \
-    std::function<void()> after_func = []() {};                     \
-    auto execute_func = [&]() {                                     \
-        res = AOPUtility::GetExecuteFuncValue<ReturnType>([&]() {   \
-            return func(__VA_ARGS__);                               \
-        });                                                         \
+#define AOP_DECLARE_FUNC_BEGIN(ContextType, func, ...)                     \
+    using ReturnType = std::decay_t<decltype(func(__VA_ARGS__))>;          \
+    auto res = ::aop::AOPUtility::GetInitValue<ReturnType>();              \
+    auto ctx = ContextType{};                                              \
+                                                                           \
+    [[maybe_unused]] auto proxy_args_func = std::invoke([&]() {            \
+        if constexpr (std::is_void_v<ReturnType>) {                        \
+            return ::aop::AOPUtility::GetProxyFunc(ctx, __VA_ARGS__);      \
+        } else {                                                           \
+            return ::aop::AOPUtility::GetProxyFunc(ctx, res, __VA_ARGS__); \
+        }                                                                  \
+    });                                                                    \
+                                                                           \
+    std::function<void()> before_func = []() {};                           \
+    std::function<void()> after_func = []() {};                            \
+    auto execute_func = [&]() {                                            \
+        res = ::aop::AOPUtility::GetExecuteFuncValue<ReturnType>([&]() {   \
+            return func(__VA_ARGS__);                                      \
+        });                                                                \
     };
 
-#define AOP_DECLARE_FUNC_ASPECT(aspect)                                                   \
-    {                                                                                     \
-        using AspectType = std::remove_pointer_t<decltype(aspect.BuildPtr())>;            \
-        std::shared_ptr<AspectType> a = nullptr;                                          \
-        a.reset(aspect.BuildPtr());                                                       \
-                                                                                          \
-        before_func = AOPUtility::ConcatFuncByQueue(before_func,                          \
-                                                    proxy_args_func([a](auto&&... args) { \
-                                                        a->Before(args...);               \
-                                                    }),                                   \
-                                                    ctx);                                 \
-                                                                                          \
-        after_func = AOPUtility::ConcatFuncByStack(after_func,                            \
-                                                   proxy_args_func([a](auto&&... args) {  \
-                                                       a->After(args...);                 \
-                                                   }),                                    \
-                                                   ctx);                                  \
+#define AOP_DECLARE_FUNC_ASPECT(aspect)                                                          \
+    {                                                                                            \
+        using AspectType = std::remove_pointer_t<decltype(aspect.BuildPtr())>;                   \
+        std::shared_ptr<AspectType> a = nullptr;                                                 \
+        a.reset(aspect.BuildPtr());                                                              \
+                                                                                                 \
+        before_func = ::aop::AOPUtility::ConcatFuncByQueue(before_func,                          \
+                                                           proxy_args_func([a](auto&&... args) { \
+                                                               a->Before(args...);               \
+                                                           }),                                   \
+                                                           ctx);                                 \
+                                                                                                 \
+        after_func = ::aop::AOPUtility::ConcatFuncByStack(after_func,                            \
+                                                          proxy_args_func([a](auto&&... args) {  \
+                                                              a->After(args...);                 \
+                                                          }),                                    \
+                                                          ctx);                                  \
     }
 
 #define AOP_DECLARE_FUNC_END()                      \
