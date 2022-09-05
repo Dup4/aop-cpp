d80f8f87a6cafe757b53daae4245bfdf011454c9
Dup4 <lyuzhi.pan@gmail.com>
2022-09-05 15:29:56 +0800

    feat: use AspectGuard

--- include/aop/aop.h
+++ include/aop/aop.h
@@ -1,6 +1,7 @@
 #ifndef AOP_AOP_H
 #define AOP_AOP_H
 
+#include "./aop_aspect_guard.h"     // IWYU pragma: export
 #include "./aop_execute_context.h"  // IWYU pragma: export
 #include "./aop_utility.h"          // IWYU pragma: export
 #include "./macros.h"               // IWYU pragma: export
