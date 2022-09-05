cdfa4799e20f0f0558338595c8d490a926df846a
Dup4 <lyuzhi.pan@gmail.com>
2022-09-05 12:33:31 +0800

    fix: remove &&

--- include/aop/aop_utility.h
+++ include/aop/aop_utility.h
@@ -41,7 +41,7 @@ public:
     }
 
     template <typename PreFunc, typename Func, typename Context>
-    static auto ConcatFuncByQueue(PreFunc&& pre_func, Func&& func, [[maybe_unused]] Context& ctx) {
+    static auto ConcatFuncByQueue(PreFunc pre_func, Func func, [[maybe_unused]] Context& ctx) {
         return [pre_func, func]() {
             pre_func();
             func();
@@ -49,7 +49,7 @@ public:
     }
 
     template <typename PreFunc, typename Func, typename Context>
-    static auto ConcatFuncByStack(PreFunc&& pre_func, Func&& func, [[maybe_unused]] Context& ctx) {
+    static auto ConcatFuncByStack(PreFunc pre_func, Func func, [[maybe_unused]] Context& ctx) {
         return [pre_func, func]() {
             func();
             pre_func();
